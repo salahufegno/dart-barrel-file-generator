1bed1cf94a348638b5960d43f7d2f3acbeb60d0e
Miquel de Domingo <miquelddg@gmail.com>
2025-03-21 08:24:12 +0100

    feat(cli): add quiet option

---
+++ .changeset/dry-bees-pump.md
@@ -0,0 +1,5 @@
+---
+"@dbfg/cli": patch
+---
+
+Add `-q, --quiet` option
--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -103,6 +103,7 @@ program
   .option('-r, --recursive', 'Generate barrel files recursively for all nested directories')
   .option('-c, --config <string>', 'Path to configuration file', undefined)
   .option('-n, --default-barrel-name <name>', 'Default name for barrel files', '')
+  .option('-q, --quiet', 'Hide all logs', false)
   .option('--excluded-dirs <dirs...>', 'Comma-separated list of directories to exclude', [])
   .option('--excluded-files <files...>', 'Comma-separated list of files to exclude', [])
   .option('--exclude-freezed', 'Exclude freezed files', false)
@@ -111,16 +112,25 @@ program
   .option('--append-folder-name', 'Append folder name to barrel file name', false)
   .option('--prepend-folder-name', 'Prepend folder name to barrel file name', false)
   .option('--prepend-package', 'Prepend package name to exports in lib folder', false)
-  .action(async (directory, { config, recursive, subfolders, ...opts }) => {
+  .action(async (directory, { config, quiet, recursive, subfolders, ...opts }) => {
     const configFile = v.parse(ConfigPath, config);
     const type = recursive ? 'RECURSIVE' : subfolders ? 'REGULAR_SUBFOLDERS' : 'REGULAR';
+
+    if (quiet) {
+      Object.assign(cliLogger, {
+        done: () => {},
+        error: () => {},
+        log: () => {},
+        warn: () => {}
+      });
+    }
 
     try {
       const dir = v.parse(Directory, directory);
       const resolvedPath = path.resolve(dir);
 
       if (!fs.existsSync(resolvedPath)) {
-        console.error(`Error: Directory does not exist: ${resolvedPath}`);
+        cliLogger.error(`Error: Directory does not exist: ${resolvedPath}`);
         process.exit(1);
       }
 
